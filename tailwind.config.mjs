c916beb846eb86e54d0e37650b7f66230ca73515
Berzan <berzan@tutanota.com>
2024-01-01 15:41:06 +0000

    upload: all the files

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				emoji: "'Noto Color Emoji', sans-serif"
+			},
+			colors: {
+				'jk-black': '#131313',
+				'jk-purple': '#7342dc',
+				'jk-white': '#ffffff'
+			}
+		}
+	},
+	plugins: []
+}
